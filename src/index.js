5342e1d5c6c020567c25d2b58d4aebb92e832e05
Harshit <harshit.shrivastava@leadsquared.com>
2020-07-11 18:14:35 +0530

    Add router and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+let Home = () => <App />;
+let Dashboard = () => <div>Displaying Dashboard</div>;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Home path="/" />
+    <Dashboard path="dashboard" />
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
