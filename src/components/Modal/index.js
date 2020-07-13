e13cd6603551d4d3613fd3a4fef316e6c0afa3c2
RAJAT VIJAY <rajatvijay5@gmail.com>
2020-07-13 14:52:35 +0530

    retaining focus

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,34 +2,56 @@ import React, { useEffect, createRef } from "react";
 import { createPortal } from "react-dom";
 import "./index.css";
 
+const FOCUSABLE_ELEMENTS = [
+  "a[href]",
+  "area[href]",
+  'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+  "select:not([disabled]):not([aria-hidden])",
+  "textarea:not([disabled]):not([aria-hidden])",
+  "button:not([disabled]):not([aria-hidden])",
+  "iframe",
+  "object",
+  "embed",
+  "[contenteditable]",
+  '[tabindex]:not([tabindex^="-"])',
+];
+
 export default function Modal({ children, onModalClose, isOpen }) {
+  const modalRef = createRef();
+
   useEffect(() => {
     function keyListener(e) {
       const listener = keyListenersMap.get(e.keyCode);
       return listener && listener(e);
     }
+
     document.addEventListener("keydown", keyListener);
 
     return () => document.removeEventListener("keydown", keyListener);
   });
 
-  const modalRef = createRef();
   const handleTabKey = (e) => {
     if (isOpen) {
-      const focusableModalElements = modalRef.current.querySelectorAll(
-        'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
-      );
-      const firstElement = focusableModalElements[0];
-      const lastElement =
-        focusableModalElements[focusableModalElements.length - 1];
+      // 1. Querying all the focusable nodes from the modal
+      // 2. Filtering out the focusable nodes that are display:none
+      const focusableNodes = Array(
+        ...modalRef.current.querySelectorAll(FOCUSABLE_ELEMENTS) // 1
+      ).filter((node) => node.offsetParent !== null); // 2
 
-      if (!e.shiftKey && document.activeElement !== firstElement) {
-        firstElement.focus();
-        return e.preventDefault();
+      if (!focusableNodes.length) {
+        return;
       }
 
-      if (e.shiftKey && document.activeElement !== lastElement) {
-        lastElement.focus();
+      const firstFocusableNode = focusableNodes[0];
+      const lastFocusableNode = focusableNodes[focusableNodes.length - 1];
+
+      if (!e.shiftKey && document.activeElement === lastFocusableNode) {
+        firstFocusableNode.focus();
+        e.preventDefault();
+      }
+
+      if (e.shiftKey && document.activeElement === firstFocusableNode) {
+        lastFocusableNode.focus();
         e.preventDefault();
       }
     }
@@ -39,6 +61,7 @@ export default function Modal({ children, onModalClose, isOpen }) {
     [27, onModalClose],
     [9, handleTabKey],
   ]);
+
   if (isOpen) {
     return createPortal(
       <div className="modal-container" role="dialog" aria-modal="true">
@@ -48,6 +71,7 @@ export default function Modal({ children, onModalClose, isOpen }) {
       </div>,
       document.body
     );
+  } else {
+    return null;
   }
-  return null;
 }
